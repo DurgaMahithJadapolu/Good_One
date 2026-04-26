a7bf794ed1805ba85e107875997c5679863be1ca
DurgaMahithJadapolu <158590615+DurgaMahithJadapolu@users.noreply.github.com>
2026-04-26 11:11:37 +0530

    CROS_Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: ['http://localhost:3000', 'https://good-one.onrender.com'],
     methods: ['GET', 'POST'],
     credentials: true
   }
